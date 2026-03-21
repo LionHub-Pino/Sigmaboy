8926e54ae00d98ebf0f5d0512dd50f52ea96b00c
Pino <fluxusvip@gmail.com>
2026-03-22 06:15:51 +0700

    Update TrackFree.lua

--- TrackFree.lua
+++ TrackFree.lua
@@ -1,6 +1,6 @@
 -- [[ CONFIG ]]
 local UPDATE_INTERVAL = 30 -- Thời gian chờ giữa mỗi lần gửi (giây)
-local WORKER_URL = "https://trackeraccount.binhgoldtt1.workers.dev/"
+local WORKER_URL = "https://trackeraccount.binhgoldtt1.workers.dev/update"
 
 -- [[ SERVICES ]]
 local http_request = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
